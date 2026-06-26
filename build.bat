ee03f3715bdf8d368489d7b5fbafef05b364b827
HuaMouRen <2825338723@qq.com>
2026-06-26 17:34:25 +0800

    build: 添加Debug构建

--- build.bat
+++ build.bat
@@ -3,6 +3,8 @@
 :: 替换为你自己的msbuild路径
 "C:\Program Files\Microsoft Visual Studio\18\Community\MSBuild\Current\Bin\MSBuild.exe" SickoMenu.vcxproj -property:Configuration=Release -property:Platform=x86 -p:AdditionalOptions="/utf-8 %%%%(AdditionalOptions)" /m
 "C:\Program Files\Microsoft Visual Studio\18\Community\MSBuild\Current\Bin\MSBuild.exe" SickoMenu.vcxproj -property:Configuration=Release_Version -property:Platform=x86 -p:AdditionalOptions="/utf-8 %%%%(AdditionalOptions)" /m
+"C:\Program Files\Microsoft Visual Studio\18\Community\MSBuild\Current\Bin\MSBuild.exe" SickoMenu.vcxproj -property:Configuration=Debug -property:Platform=x86 -p:AdditionalOptions="/utf-8 %%%%(AdditionalOptions)" /m
+"C:\Program Files\Microsoft Visual Studio\18\Community\MSBuild\Current\Bin\MSBuild.exe" SickoMenu.vcxproj -property:Configuration=Debug_Version -property:Platform=x86 -p:AdditionalOptions="/utf-8 %%%%(AdditionalOptions)" /m
 
 :: 复制成品至游戏目录 方便调试（你可以删除这段代码）
 copy "Release\SickoMenu.dll" "Z:\Steam\steamapps\common\Among Us"
