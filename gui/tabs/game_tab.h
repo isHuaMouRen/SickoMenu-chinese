d45abda4354fdf386f464723318e8a80f3cfd0c3
HuaMouRen <2825338723@qq.com>
2026-08-12 18:23:29 +0800

    feat: 汉化颜色身份等信息

--- gui/tabs/game_tab.h
+++ gui/tabs/game_tab.h
@@ -3,13 +3,13 @@
 #include "state.hpp"
 
 namespace GameTab {
-	const std::vector<const char*> KILL_DISTANCE = { "Short", "Medium", "Long", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20" };
-	const std::vector<const char*> TASKBARUPDATES = { "Always", "Meetings", "Never", "3", "4", "5", "6" };
-	const std::vector<const char*> COLORS = { "Red", "Blue", "Green", "Pink", "Orange", "Yellow", "Black", "White", "Purple", "Brown", "Cyan", "Lime", "Maroon", "Rose", "Banana", "Gray", "Tan", "Coral" };
-	const std::vector<const char*> HOSTCOLORS = { "Red", "Blue", "Green", "Pink", "Orange", "Yellow", "Black", "White", "Purple", "Brown", "Cyan", "Lime", "Maroon", "Rose", "Banana", "Gray", "Tan", "Coral", "Fortegreen"};
-	const std::vector<const char*> SMAC_PUNISHMENTS = { "Do Nothing", "Warn Self"/*, "Warn All (Chat)", State.SafeMode ? "Attempt to Ban" : "Attempt to Kick"*/};
-	const std::vector<const char*> SMAC_HOST_PUNISHMENTS = { "Do Nothing", "Warn Self"/*, "Warn All (Chat)"*/, "Kick", "Ban"};
-	const std::vector<const char*> SHIPVENTS = { "Admin", "Hallway", "Cafeteria", "Electrical", "Upper Engine", "Security", "Medbay", "Weapons", "Lower Reactor", "Lower Engine", "Shields", "Upper Reactor", "Upper Navigation", "Lower Navigation" };
+	const std::vector<const char*> KILL_DISTANCE = { "短", "中", "长", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20" };
+	const std::vector<const char*> TASKBARUPDATES = { "总是", "开会", "从不", "3", "4", "5", "6" };
+	const std::vector<const char*> COLORS = { "红色", "蓝色", "绿色", "粉色", "橘色", "黄色", "黑色", "白色", "紫色", "棕色", "青色", "黄绿色", "红褐色", "玫红色", "焦黄色", "灰色", "茶色", "珊瑚色" };
+	const std::vector<const char*> HOSTCOLORS = { "红色", "蓝色", "绿色", "粉色", "橘色", "黄色", "黑色", "白色", "紫色", "棕色", "青色", "黄绿色", "红褐色", "玫红色", "焦黄色", "灰色", "茶色", "珊瑚色", "深绿色"};
+	const std::vector<const char*> SMAC_PUNISHMENTS = { "什么都不做", "警告自己"/*, "Warn All (Chat)", State.SafeMode ? "Attempt to Ban" : "Attempt to Kick"*/};
+	const std::vector<const char*> SMAC_HOST_PUNISHMENTS = { "什么都不做", "警告自己"/*, "Warn All (Chat)"*/, "Kick", "Ban"};
+	const std::vector<const char*> SHIPVENTS = { "管理室", "走廊", "餐厅", "电力室", "上引擎室", "安保室", "医疗室", "武器室", "下反应堆", "下引擎", "护盾室", "上反应堆", "上导航室", "下导航室" };
 	const std::vector<const char*> HQVENTS = { "Balcony", "Cafeteria", "Reactor", "Laboratory", "Office", "Admin", "Greenhouse", "Medbay", "Decontamination", "Locker Room", "Launchpad" };
 	const std::vector<const char*> PBVENTS = { "Security", "Electrical", "O2", "Communications", "Office", "Admin", "Laboratory", "Lava Pool", "Storage", "Right Seismic", "Left Seismic", "Outside Admin" };
 	const std::vector<const char*> AIRSHIPVENTS = { "Vault", "Cockpit", "Viewing Deck", "Engine", "Kitchen", "Lower Main Hall", "Upper Main Hall", "Right Gap Room", "Left Gap Room", "Showers", "Records", "Cargo Bay" };
--- gui/tabs/players_tab.h
+++ gui/tabs/players_tab.h
@@ -1,10 +1,10 @@
 #pragma once
 
 namespace PlayersTab {
-	const std::vector<const char*> FAKEROLES = { "Crewmate", "Impostor", "Scientist", "Engineer", "Guardian Angel", "Shapeshifter", "Crewmate Ghost", "Impostor Ghost", "Noisemaker", "Phantom", "Tracker", "Detective", "Viper" };
-	const std::vector<const char*> GHOSTROLES = { "Guardian Angel", "Crewmate Ghost", "Impostor Ghost" };
-	const std::vector<const char*> COLORS = { "Red", "Blue", "Green", "Pink", "Orange", "Yellow", "Black", "White", "Purple", "Brown", "Cyan", "Lime", "Maroon", "Rose", "Banana", "Gray", "Tan", "Coral", "Fortegreen" };
-	const std::vector<const char*> SHIPVENTS = { "Admin", "Hallway", "Cafeteria", "Electrical", "Upper Engine", "Security", "Medbay", "Weapons", "Lower Reactor", "Lower Engine", "Shields", "Upper Reactor", "Upper Navigation", "Lower Navigation" };
+	const std::vector<const char*> FAKEROLES = { "船员", "伪装者", "科学家", "工程师", "守护天使", "变形者", "船员幽灵", "伪装者幽灵", "大嗓门", "幻术师", "追踪者", "侦探", "毒蛇" };
+	const std::vector<const char*> GHOSTROLES = { "守护天使", "船员幽灵", "伪装者幽灵" };
+	const std::vector<const char*> COLORS = { "红色", "蓝色", "绿色", "粉色", "橘色", "黄色", "黑色", "白色", "紫色", "棕色", "青色", "黄绿色", "红褐色", "玫红色", "焦黄色", "灰色", "茶色", "珊瑚红", "深绿色" };
+	const std::vector<const char*> SHIPVENTS = { "管理室", "走廊", "餐厅", "电力室", "上引擎", "安保室", "医务室", "武器室", "下反应堆", "下引擎", "护盾室", "上反应堆", "上导航室", "下导航室" };
 	const std::vector<const char*> HQVENTS = { "Balcony", "Cafeteria", "Reactor", "Laboratory", "Office", "Admin", "Greenhouse", "Medbay", "Decontamination", "Locker Room", "Launchpad" };
 	const std::vector<const char*> PBVENTS = { "Security", "Electrical", "O2", "Communications", "Office", "Admin", "Laboratory", "Lava Pool", "Storage", "Right Seismic", "Left Seismic", "Outside Admin" };
 	const std::vector<const char*> AIRSHIPVENTS = { "Vault", "Cockpit", "Viewing Deck", "Engine", "Kitchen", "Lower Main Hall", "Upper Main Hall", "Right Gap Room", "Left Gap Room", "Showers", "Records", "Cargo Bay" };
